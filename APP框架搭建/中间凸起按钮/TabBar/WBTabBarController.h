3b554520d39b9e9a59c15aaf53ecb4f703d79fab
wenbo <bo.wen@cyzone.cn>
2018-07-04 15:15:39 +0800

    提交框架大家demo

---
+++ APP框架搭建/中间凸起按钮/TabBar/WBTabBarController.h
@@ -0,0 +1,15 @@
+//
+//  WBTabBarController.h
+//  WBAppFrameWorkDemo
+//
+//  Created by WMB on 2017/10/10.
+//  Copyright © 2017年 WMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBTabBarController : UITabBarController
+
+- (void)setupUnreadMessageCount;
+
+@end
